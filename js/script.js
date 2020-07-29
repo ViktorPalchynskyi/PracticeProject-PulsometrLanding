5761f6fe51a9068625ed6d78d8da8fc8f3b5533a
Arlecuno <palchinskij19@gmail.com>
2020-07-29 23:52:37 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function () {
    $('.carousel__inner').slick({
-      speed: 1000,
+      speed: 500,
       cssEase: 'linear',
-      nextArrow: `<button type="button" class="slick-next"><img src="../img/slider/right_arrow.png" alt="" /></button>`,
-      prevArrow: `<button type="button" class="slick-prev"><img src="../img/slider/left_arrow.png" alt="" /></button>`,
+      fade: true,
+      nextArrow: `<button type="button" class="slick-next"><img src="img/slider/right_arrow.png" alt="" /></button>`,
+      prevArrow: `<button type="button" class="slick-prev"><img src="img/slider/left_arrow.png" alt="" /></button>`,
       responsive: [
          {
             breakpoint: 768,
@@ -14,4 +15,21 @@ $(document).ready(function () {
             }
          }]
    });
+   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+      $(this)
+         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+   });
+
+   function toggleSlide(item) {
+      $(item).each(function (i) {
+         $(this).on('click', e => {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+         })
+      });
+   };
+   toggleSlide('.catalog-item__link')
+   toggleSlide('.catalog-item__back')
 }); 
