bc9f024933e3e79a5438845dcae3203fa42239d6
Arlecuno <palchinskij19@gmail.com>
2020-08-01 21:28:27 +0300

    add scroll

--- js/script.js
+++ js/script.js
@@ -86,4 +86,16 @@ $(document).ready(function () {
       });
       return false;
    });
+   // Scroll
+   $(window).scroll(() => {
+      if ($(this).scrollTop() > 1600) {
+         $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();
+      }
+   }); $("a[href^='#']").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      return false;
+   });
 }); 
